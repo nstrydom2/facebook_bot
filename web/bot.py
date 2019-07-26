1ea655a3a38d2f83c970d00fc565e2b955c5fda1
Nick Strydom <nstrydom@gmail.com>
2019-07-26 11:13:50 -0500

    Through together TestCase for Bot()

--- web/bot.py
+++ web/bot.py
@@ -1,14 +1,41 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.proxy import Proxy, ProxyType
 
+from config import proxy as proxy_conf
 
 class Bot():
-    def __init__(self):
+    def __init__(self, proxy=False):
         # Prob create selenium instance here
-        self.driver = webdriver.Firefox(executable_path=r'/home/ghost/Drivers/geckodriver')
+        if proxy is False:
+            self.driver = webdriver.Firefox(executable_path=r'/home/ghost/Drivers/geckodriver')
+        else:
+            # Set up proxy if 'proxy' variables' value is True
+            proxy_server = Proxy()
+            proxy_server.proxy_type = ProxyType.MANUAL
+            proxy_server.http_proxy = proxy_conf.LIST['http']
+            proxy_server.socks_proxy = proxy_conf.LIST['sock']
+            proxy_server.ssl_proxy = proxy_conf.LIST['ssl']
+
+            capabilities = webdriver.DesiredCapabilities.FIREFOX
+            proxy_server.add_to_capabilities(capabilities)
+
+            self.driver = webdriver.Firefox(executable_path=r'/home/ghost/Drivers/geckodriver',
+                                            desired_capabilities=capabilities)
 
     def load_fb(self):
         self.driver.get('https://facebook.com/')
+        self.wait()
+
+    def load_wall(self):
+        self.driver.find_element_by_xpath(
+            '/html/body/div[1]/div[2]/div/div[1]/div/div/div/div[1]/div[1]/h1/a/span').click()
+        self.wait()
+
+    def load_profile(self):
+        self.driver.find_element_by_xpath(
+            '/html/body/div[1]/div[2]/div/div[1]/div/div/div/div[2]/div[1]/div[1]/div/a'
+        ).click()
         self.wait()
 
     # Login to facebook
@@ -42,8 +69,8 @@ class Bot():
             for friend in range(1, limit):
                 # Click on 'Add Friend' button of recommended friend
                 self.driver.find_element_by_xpath(
-                    '/html/body/div[1]/div[3]/div[1]/div/div[1]/div[2]/div[2]/div/form/div/div/div[1]/ul[1]/li[' /
-                    + str(friend) + ']/div/div/div/div[2]/div[2]/div/div/div/div/div/button[1]'
+                    '/html/body/div[1]/div[3]/div[1]/div/div[1]/div[2]/div[2]/div/form/div/div/div[1]/ul[1]/li[' +
+                    str(friend) + ']/div/div/div/div[2]/div[2]/div/div/div/div/div/button[1]'
                 ).click()
                 self.wait()
         except:
@@ -53,6 +80,23 @@ class Bot():
     def accept_all_friend_requests(self):
         self.load_friends_requests()
         pass
+
+    def upload_picture(self, caption='', img_path=''):
+        self.driver.find_element_by_xpath(
+            '/html/body/div[1]/div[3]/div[1]/div/div[2]/div[2]/div[1]/div[2]/div/div[3]/div/div' +
+            '/div[2]/div/div/div/div/div[2]/div/div[2]/div[2]/ul/li[1]/div/div/span/a'
+        ).click()
+        self.wait()
+
+        file_upload = self.driver.find_element_by_xpath('//*[@id="js_5g2"]')
+        file_upload.send_keys(img_path)
+
+        status_text_in = self.driver.find_element_by_xpath(
+            '/html/body/div[1]/div[3]/div[1]/div/div[2]/div[2]/div[1]/div[2]/div/div[3]/div/div/div[2]/div[1]' +
+            '/div/div/div/div[2]/div/div[1]/div[1]/div/div[1]/div[2]/div/div/div/div/div/div/div/div'
+        )
+        status_text_in.send_keys(caption)
+        status_text_in.send_keys(Keys.RETURN)
 
     def wait(self, delay=3):
         self.driver.implicitly_wait(delay)
--- web/bot_test.py
+++ web/bot_test.py
@@ -18,3 +18,46 @@ class Bot_TestCase(TestCase):
         finally:
             time.sleep(3)
             bot.close()
+
+    def test_load_facebook(self):
+        bot = Bot()
+
+        try:
+            bot.load_fb()
+
+        except Exception as ex:
+            print(ex)
+
+        finally:
+            time.sleep(3)
+            bot.close()
+
+    def test_load_wall(self):
+        bot = Bot()
+
+        try:
+            bot.load_fb()
+            bot.login(acct.USER, acct.PASS)
+            bot.load_wall()
+
+        except Exception as ex:
+            print(ex)
+
+        finally:
+            time.sleep(3)
+            bot.close()
+
+    def test_upload_pic(self):
+        bot = Bot()
+
+        try:
+            bot.load_fb()
+            bot.login(acct.USER, acct.PASS)
+            bot.upload_picture(caption='Test upload', img_path='pictures/profile_pic.jpg')
+
+        except Exception as ex:
+            print(ex)
+
+        finally:
+            time.sleep(3)
+            bot.close()
